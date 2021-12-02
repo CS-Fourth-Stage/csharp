cac1f67e031995023ac1ac2258916dd2d9e28cf4
Hozan Ageed <57224535+hozan66@users.noreply.github.com>
2021-12-03 02:44:02 +0300

    Muhammad Jafer

---
+++ Algorithm Usin C#/Quick_Sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        static int pa(int[] A, int start, int end) {
+            int temp = 0;
+            int pivot = A[end];
+            int pi = start;
+            for (int i = start; i < end; i++)
+            {
+                if(A[i] <= pivot)
+                {
+                    temp = A[i];
+                    A[i] = A[pi];
+                    A[pi] = temp;
+                    pi++;
+                }
+            }
+            temp = A[pi];
+            A[pi] = A[end];
+            A[end] = temp;
+            return pi;
+        }
+
+        static void QuickSort(int[] A, int start, int end) {
+            if (start < end) {
+                int pi = pa(A, start, end);
+                QuickSort(A, start, pi-1);
+                QuickSort(A, pi+1, end);
+            }
+        }
+
+        static void Main()
+        {
+            int[] A = new int[5] { 7, 2, 3, 6, 0 };
+            QuickSort(A, 0, 4);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
